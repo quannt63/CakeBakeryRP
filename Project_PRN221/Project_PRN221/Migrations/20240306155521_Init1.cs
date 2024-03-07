cae11e60f0dc9bee752e73653d5f5951ea89c179
quannt63 <141246383+quannt63@users.noreply.github.com>
2024-03-07 21:58:30 +0700

    add
    add project

---
+++ Project_PRN221/Project_PRN221/Migrations/20240306155521_Init1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project_PRN221.Migrations
+{
+    public partial class Init1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Users",
+                type: "nvarchar(400)",
+                maxLength: 400,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Users");
+        }
+    }
+}
